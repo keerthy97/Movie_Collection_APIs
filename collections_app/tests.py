f17485e11469645e26239e55a3564abd7f3a481f
keerthy97 <46563727+keerthy97@users.noreply.github.com>
2023-11-17 14:15:37 +0530

    Update tests.py

--- collections_app/tests.py
+++ collections_app/tests.py
@@ -1,22 +1,83 @@
-from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
+import factory
+from factory.django import DjangoModelFactory
+from django.contrib.auth.models import User
+from .models import Movie, Collection, MovieCollection
 from rest_framework.test import APIClient
 from rest_framework import status
-from django.contrib.auth.models import User
-from .models import Collection, Movie
+from django.test import TestCase
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    password = factory.PostGenerationMethodCall('set_password', 'password123')
+
+class MovieFactory(DjangoModelFactory):
+    class Meta:
+        model = Movie
+
+    title = factory.Faker('sentence')
+    description = factory.Faker('text')
+    genres = factory.Faker('word')
+
+class CollectionFactory(DjangoModelFactory):
+    class Meta:
+        model = Collection
+
+    title = factory.Faker('sentence')
+    description = factory.Faker('text')
+    user = factory.SubFactory(UserFactory)
+
+class MovieCollectionFactory(DjangoModelFactory):
+    class Meta:
+        model = MovieCollection
+
+    collection = factory.SubFactory(CollectionFactory)
+    movie = factory.SubFactory(MovieFactory)
 
 class CollectionTests(TestCase):
     def setUp(self):
         # Create a test user
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user = UserFactory()
 
         # Create a test movie
-        self.movie = Movie.objects.create(title='Test Movie', description='Test Description', genres='Action')
+        self.movie = MovieFactory()
 
         # Create a test collection
-        self.collection = Collection.objects.create(title='Test Collection', description='Test Description', user=self.user)
+        self.collection = CollectionFactory(user=self.user)
+        self.collection.movies.add(self.movie)
+
+        # Set up the client for making API requests
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_collection(self):
+        # Use the factories to generate fake data
+        user = UserFactory()
+        movie = MovieFactory()
+        
+        # Create a collection with the generated data
+        data = {
+            'title': 'Test Collection',
+            'description': 'Test Description',
+            'movies': [{'title': 'Test Movie', 'description': 'Test Description', 'genres': 'Action'}],
+            'user': user.id
+        }
+
+        response = self.client.post('/collection/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class CollectionTests(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = UserFactory()
+
+        # Create a test movie
+        self.movie = MovieFactory()
+
+        # Create a test collection
+        self.collection = CollectionFactory(user=self.user)
         self.collection.movies.add(self.movie)
 
         # Set up the client for making API requests
@@ -30,7 +91,11 @@ class CollectionTests(TestCase):
 
     def test_update_collection(self):
         url = f'/collection/{self.collection.uuid}/'
-        data = {'title': 'Updated Collection', 'description': 'Updated Description', 'movies': [{'title': 'Updated Movie', 'description': 'Updated Description', 'genres': 'Drama'}]}
+        data = {
+            'title': 'Updated Collection',
+            'description': 'Updated Description',
+            'movies': [{'title': 'Updated Movie', 'description': 'Updated Description', 'genres': 'Drama'}]
+        }
         response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -39,4 +104,3 @@ class CollectionTests(TestCase):
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Collection.objects.filter(uuid=self.collection.uuid).exists())
-
